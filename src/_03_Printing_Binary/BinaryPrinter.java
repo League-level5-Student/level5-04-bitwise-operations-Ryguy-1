e91dfb4cc4c65e30ae8329a0f486b03f82e57c2f
League Student <league@iMac-12.attlocal.net>
2019-12-27 16:53:01 -0800

    very hard plz help mucho

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -8,11 +8,28 @@ public class BinaryPrinter {
 	//Create a main method to test your methods.
 	
 	public void printByteBinary(byte b) {
-	
+		System.out.println(b>>0);
+		String counter ="";
+		//need a way to figure out how to change 0x00000001 thing!!! -- may be able to divide by ten or something
+		for (int i = 8; i > 0; i--) {
+			if((0x00000001 & b)>>i==1) {
+				counter += (1 + "");
+			}
+		}
+		System.out.println(counter);
+		
 	}
 	
 	public void printShortBinary(short s) {
-		
+		//16 bit
+//		int sign = Integer.signum(s);
+//		if (sign==1) {
+//			for (int i = 0; i < 15; i++) {
+//				if(temp<) {
+//					
+//				}
+//			}
+//		}
 	}
 	
 	public void printIntBinary(int i) {
@@ -25,7 +42,9 @@ public class BinaryPrinter {
 	
 	
 	public static void main(String[] args) {
-		
+		BinaryPrinter printer = new BinaryPrinter();
+		byte s = 122;
+		printer.printByteBinary(s);
 	}
 	
 	
